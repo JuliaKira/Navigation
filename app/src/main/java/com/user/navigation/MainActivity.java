19a31eb34e1dff592e99dda0b1d617dc1629a3c6
JuliKira <k148a@mail.ru>
2023-09-20 18:02:03 +0300

    Initial commit

---
+++ app/src/main/java/com/user/navigation/MainActivity.java
@@ -0,0 +1,22 @@
+package com.user.navigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        NavController navController = Navigation.findNavController(this, R.id.my_nav_host_fragment);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+}

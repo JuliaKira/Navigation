4d01154b69faa6285b2522a23b6206f9557b7233
safronov <ilya.safa1337w@gmail.com>
2023-09-22 08:59:31 +0500

    Added comments

--- app/src/main/java/com/user/navigation/MainActivity.java
+++ app/src/main/java/com/user/navigation/MainActivity.java
@@ -11,6 +11,8 @@ import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+/* 🖤 */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -18,11 +20,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //TODO поставь блок try { ... } catch { ... } - что бы логировать исключения
+
+        /* Получаем наш граф */
         NavHostFragment fragmentById = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.my_nav_host_fragment);
         assert fragmentById != null;
+
+        /* Инитим navController */
         NavController navController = fragmentById.getNavController();
+
+        /* Делаем setup */
         BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
     }
+
 }
